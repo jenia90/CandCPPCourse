5cf341684476c08ae4ffcf17037d64eaf6edfedb
Jenia <genia90@gmail.com>
2016-09-19 19:26:45 +0300

    some docs for ex3

--- CPP_Course/ex3/Matrix.hpp
+++ CPP_Course/ex3/Matrix.hpp
@@ -9,9 +9,9 @@
 #define CELL_DELIM "\t"
 
 template <typename T>
-class Matrix
+class Matrix<>
 {
-    unsigned int _rows, _cols;
+    size_t _rows, _cols;
     std::vector<T> _cells;
 
     /**
@@ -36,45 +36,90 @@ class Matrix
         return true;
     }
 public:
-    Matrix() : _rows(1), _cols(1), _cells(0)
-    {
-    }
-    Matrix(unsigned int rows, unsigned int cols) : _rows(rows), _cols(cols)
-    {
-    }
-
-    Matrix(unsigned int rows, unsigned int cols, const std::vector<T>& cells) : _rows(rows),
-                                                                                _cols(cols),
-                                                                                _cells(cells)
+    /**
+     * @brief Default ctor. Creates a 1x1 Matrix with 0 in its only cell.
+     * @return
+     */
+    Matrix<>() : _rows(1), _cols(1), _cells(1, 0)
     {
     }
 
-    Matrix(Matrix&& matrix) : _rows(std::move(matrix._rows)), _cols(std::move(matrix._cols)),
+    /**
+     * @brief Ctor to init a matrix of size rows x cols with 0 in all its cells
+     * @param rows number of rows
+     * @param cols number of cols
+     * @return
+     */
+    Matrix<>(size_t rows, size_t cols) : _rows(rows), _cols(cols), _cells(rows * cols, 0)
+    {
+    }
+
+    /**
+     * @brief Ctor to init a matrix of size rows x cols with data contained in the cells vector
+     * @param rows number of rows
+     * @param cols number of columns
+     * @param cells cells data vector
+     * @return
+     */
+    Matrix<>(size_t rows, size_t cols, const std::vector<T>& cells) : _rows(rows), _cols(cols),
+                                                                                 _cells(cells)
+    {
+    }
+
+    /**
+     * @brief copy ctor. copies data from one matrix to the other
+     * @param matrix matrix to copy the data from
+     * @return
+     */
+    Matrix<>(const Matrix &matrix) : _rows(matrix._rows), _cols(matrix._cols), _cells(matrix._cells)
+    {
+    }
+
+    /**
+     * @brief move ctor. moves the data from one matrix to the other
+     * @param matrix the matrix to move the data from
+     * @return
+     */
+    Matrix<>(Matrix &&matrix) : _rows(std::move(matrix._rows)), _cols(std::move(matrix._cols)),
                               _cells(std::move(matrix._cells))
     {
     }
 
-
-
+    /**
+     * @brief dtor
+     */
     ~Matrix()
     {
         _cells.clear();
     }
 
+    /**
+     * @brief Checks if this matrix is square (if its row number equal to columns number)
+     * @return true if square; false otherwise
+     */
     bool isSquareMatrix()
     {
         return _rows == _cols;
     }
 
-    unsigned int rows() const
+    /**
+     * @brief Getter for the rows number
+     * @return number of rows
+     */
+    size_t rows() const
     {
         return _rows;
     }
 
-    unsigned int cols() const
+    /**
+     * @brief Getter for the columns number
+     * @return number of columns
+     */
+    size_t cols() const
     {
         return _cols;
     }
+
 
     Matrix&operator=(const Matrix & matrix)
     {
@@ -120,7 +165,23 @@ public:
 
     Matrix operator*(const Matrix& matrix)
     {
-        //TODO: Implement
+        int sum;
+        Matrix m = Matrix(_rows, matrix._cols);
+
+        for (size_t i = 0; i < _rows; ++i)
+        {
+            for (size_t j = 0; j < matrix._cols; ++j)
+            {
+                sum = 0;
+                for (size_t k = 0; k < _cols; ++k)
+                {
+                    sum += (*this)(i, k) * matrix(k, j);
+                }
+                m(i, j) = sum;
+            }
+        }
+
+        return m;
     }
 
     bool operator==(const Matrix& matrix)
@@ -133,13 +194,13 @@ public:
         return !isEqual(matrix);
     }
 
-    std::ostream& operator<<(std::ostream &os, const Matrix& matrix)
+    friend std::ostream& operator<<(std::ostream &os, const Matrix& matrix)
     {
-        for (size_t r = 0; r < _rows; ++r)
+        for (size_t r = 0; r < matrix._rows; ++r)
         {
-            for (size_t c = 0; c < _cols; ++c)
+            for (size_t c = 0; c < matrix._cols; ++c)
             {
-                os << (*this)(r, c) << CELL_DELIM;
+                os << matrix(r, c) << CELL_DELIM;
             }
             os << std::endl;
         }
@@ -157,7 +218,7 @@ public:
         return _cells[_cols * r + c];
     }
 
-    T begind()
+    T begin()
     {
         return _cells.begin();
     }
